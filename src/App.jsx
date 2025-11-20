06bd90d2251e903bc7d60d21d90787c82ab561f3
Hamid997 <hamid.birouk0597@gmail.com>
2025-11-20 23:20:14 +0100

    day 2 - first edit on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,50 @@
-import './App.css'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Skills from './components/Skills'
-import Educations from './components/Educations'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-function App() {
+import "./App.css";
+import Navbar from "./components/NavBar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Educations from "./components/Educations";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
+function App() {
   return (
     <>
-       <h1>Birko Portfolio</h1>
-       <NavBar />
-       <Hero />
-       <About />
-       <Skills />
-       <Educations />
-       <Projects />
-       <Contact />
-       <Footer />
+      <nav className="navbar">
+    <div className="navbar-brand">
+      <a>My App</a>
+    </div>
+
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <i className="nav-link">About</i>
+      </li>{" "}
+      <li className="nav-item">
+        <i className="nav-link">Skills</i>
+      </li>{" "}
+      <li className="nav-item">
+        <i className="nav-link">Education</i>
+      </li>{" "}
+      <li className="nav-item">
+        <i className="nav-link">Projects</i>
+      </li>{" "}
+      <li className="nav-item">
+        <i className="nav-link">Contact</i>
+      </li>{" "}
+      <li className="nav-item">☀ </li>
+    </ul>
+  </nav>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Educations />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,2 +1,31 @@
-export default function Navbar() {
+import "../App.css";
+
+function Navbar() {
+  <nav className="navbar">
+    <div className="navbar-brand">
+      <a>My App</a>
+    </div>
+
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <i className="nav-link">About</i>
+      </li>{" "}
+      <li className="nav-item">
+        <i className="nav-link">Skills</i>
+      </li>{" "}
+      <li className="nav-item">
+        <i className="nav-link">Education</i>
+      </li>{" "}
+      <li className="nav-item">
+        <i className="nav-link">Projects</i>
+      </li>{" "}
+      <li className="nav-item">
+        <i className="nav-link">Contact</i>
+      </li>{" "}
+      <li className="nav-item">☀ </li>
+    </ul>
+  </nav>;
 }
+
+export default Navbar;
+
